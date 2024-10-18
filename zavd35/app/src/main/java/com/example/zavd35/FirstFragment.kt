a7c6e332d1133a32cc11ae972b76b5ea86262dd5
maxim920 <reyjandplay@gmail.com>
2024-10-18 21:50:05 +0300

    moved files

---
+++ zavd35/app/src/main/java/com/example/zavd35/FirstFragment.kt
@@ -0,0 +1,29 @@
+package com.example.zavd35
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.zavd35.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnToSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+}
---
+++ zavd35/app/src/main/java/com/example/zavd35/ThirdFragment.kt
@@ -0,0 +1,30 @@
+package com.example.zavd35
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.zavd35.databinding.FragmentThirdBinding
+
+class ThirdFragment : Fragment() {
+    private lateinit var binding: FragmentThirdBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentThirdBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnToFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_thirdFragment_to_firstFragment)
+        }
+    }
+}
